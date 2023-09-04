cd17c62c1460d9fd4fd8cfa46c841c1df58664d0
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-04 13:33:03 +0530

    Head Created

--- Blog-site/src/Components/Image.jsx
+++ Blog-site/src/Components/Image.jsx
@@ -1,9 +1,9 @@
-// import Img from "../assets/gettyimages-101745121-612x612.jpg"
+import Img from "../assets/gettyimages-101745121-612x612.jpg"
 
 const Image = () => {
     return (
         <>
-            <p>Image</p>
+            <img className="m-4 rounded-2xl" src={Img} alt="" />
         </>
     )
 }
