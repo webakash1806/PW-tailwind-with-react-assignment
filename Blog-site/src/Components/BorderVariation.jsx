74548d647abb9b0241d27ffa5e0f6fdaa67ab544
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-04 13:09:36 +0530

    borderVariation component created

--- Blog-site/src/Components/BorderVariation.jsx
+++ Blog-site/src/Components/BorderVariation.jsx
@@ -1,7 +1,10 @@
 const BorderVariation = () => {
     return (
         <>
-            <p>Border</p>
+            <div className="border-l-[3px] border-black m-3 p-2 sm:m-10">
+                <p className="">&quot;The people who are crazy enough to think they can change the world are the ones who do.&quot;</p>
+                <p className="">-Apple's "Think Different" commercial, 1997</p>
+            </div>
         </>
     )
 }
