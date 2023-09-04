4cbf7c1e70970e2b7840d0f70a0baa2b5e4e1946
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-04 12:14:32 +0530

    Profile component created

--- Blog-site/src/Components/Profile.jsx
+++ Blog-site/src/Components/Profile.jsx
@@ -1,7 +1,10 @@
 const Profile = () => {
     return (
         <>
-            Profile
+            <div className="profile-card w-screen flex flex-col items-center gap-3 justify-center sm:flex-row sm:gap-5">
+                <p className="bg-[#4ADE80] w-8 h-8 flex justify-center items-center rounded-full p-7 font-semibold text-[1.7rem]">M</p>
+                <p className="text-black w-10/12 sm:w-6/12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, magni quasi quisquam beatae ab porro pariatur alias iste et, repellendus quam provident quod, ad doloremque?</p>
+            </div>
         </>
     )
 }
